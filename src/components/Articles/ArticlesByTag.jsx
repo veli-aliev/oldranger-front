0e56c6bc962e98133e35bbf85fa9b4e0e096bf71
TaitoMi <gfdaker96@gmail.com>
2020-01-19 19:41:16 +0300

    added articles output by tag

---
+++ src/components/Articles/ArticlesByTag.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Spin } from 'antd';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import queries from '../../serverQueries';
+import Article from './Article';
+
+class ArticlesByTag extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+      isEmpty: false,
+    };
+  }
+
+  componentDidMount() {
+    const {
+      match: {
+        params: { ArticleTag },
+      },
+    } = this.props;
+    queries.getArticlesByTag(ArticleTag).then(el => {
+      this.setState({ articles: el.content, isEmpty: el.empty });
+    });
+  }
+
+  render() {
+    const { articles, isEmpty } = this.state;
+    const LoadOrNotFound = isEmpty ? <h1>Статей по этому тегу не найдено</h1> : <Spin />;
+    return (
+      <>
+        {articles.length === 0 ? LoadOrNotFound : null}
+        {articles
+          .filter(el => el.hideToAnon !== true)
+          .reverse()
+          .map(el => (
+            <Article key={el.id} articleInfo={el} />
+          ))}
+      </>
+    );
+  }
+}
+
+ArticlesByTag.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.objectOf(PropTypes.string),
+  }).isRequired,
+};
+
+export default withRouter(ArticlesByTag);

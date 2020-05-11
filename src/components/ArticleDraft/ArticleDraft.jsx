1b8fe15cfe1defc9f3030b92bba1b159ba5cae3b
Safar1997 <kurbonov_safar@bk.ru>
2020-05-11 13:41:50 +0300

    refactoring

--- src/components/ArticleDraft/ArticleDraft.jsx
+++ src/components/ArticleDraft/ArticleDraft.jsx
@@ -4,32 +4,32 @@ import { Link } from 'react-router-dom';
 import querie from '../../serverQueries';
 
 const ArticleDraft = () => {
-  const draftsArray = [];
-  const [arr, update] = useState([]);
+  const [draftsArr, setDraftsArr] = useState([]);
 
   useEffect(() => {
     querie.getArticleDraft().then(res => {
       res.content.forEach(el => {
         const { id, title, text } = el;
-
-        draftsArray.push({ id, title, text });
-        update(prev => {
+        setDraftsArr(prev => {
           return [...prev, { id, title, text }];
         });
       });
     });
   }, []);
 
+  const extractionText = (startId, endId, text) => {
+    return text.substring(startId, endId);
+  };
+
   return (
     <>
       <Divider orientation="left">Черновики</Divider>
       <List
         bordered
-        dataSource={arr}
+        dataSource={draftsArr}
         renderItem={({ id, title, text }) => (
           <List.Item style={{ height: '100px', fontSize: '16px' }}>
-            {/* с сервера приходит тело статьи, обернутое в тег */}
-            <List.Item.Meta title={title} description={text.substring(3, text.length - 4)} />
+            <List.Item.Meta title={title} description={extractionText(3, text.length - 4, text)} />
             {<Link to={`/article/${id}/update`}>Редактировать</Link>}
           </List.Item>
         )}

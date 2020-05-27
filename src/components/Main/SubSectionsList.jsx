77349e846f4b7c8e40729caa34662d0f36d076f3
Safar1997 <kurbonov_safar@bk.ru>
2020-05-27 13:43:39 +0300

    shown-info-fix

--- src/components/Main/SubSectionsList.jsx
+++ src/components/Main/SubSectionsList.jsx
@@ -3,7 +3,10 @@ import TopicsList from '../Subsection/TopicsList';
 import SubSectionItem from './SubSectionItem';
 import sectionProps from './propTypes/sectionProps';
 
-const SubSectionsList = ({ section }) => {
+const SubSectionsList = ({ section, isLogin }) => {
+  if (section.section.name === 'Только для пользователей' && !isLogin) {
+    return null;
+  }
   return (
     <TopicsList
       title={section.section.name}

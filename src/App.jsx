2b14c090f515e6f14a07f00cb7d22e27ff9912a4
Safar1997 <kurbonov_safar@bk.ru>
2020-05-28 13:55:57 +0300

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ class App extends React.Component {
     if (isLogin) {
       await this.connect();
     }
-    this.isConnected();
+    this.сonnect();
   };
 
   connect = async () => {
@@ -67,13 +67,12 @@ class App extends React.Component {
     this.setState({ isJoinChat: isJoin, countMessages: 0 });
   };
 
-  isConnected = () => {
+  сonnect = () => {
     this.setState({ connect: true });
   };
 
   onConnected = () => {
     this.stompClient.subscribe(`/channel/public`, this.onCheckMessage, {});
-    this.setState({ connect: true });
   };
 
   disconnect = () => {
